cf103048ee44c0c87badb89e20569922f6c729fa
Cyrus Tau <cyrus.tau@gmail.com>
2014-05-26 21:13:54 -0700

    Initial Commit

---
+++ User Data Challenge Solution/CTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CTAppDelegate.h
+//  User Data Challenge Solution
+//
+//  Created by Monika Gorkani on 5/26/14.
+//  Copyright (c) 2014 Cyrus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ User Data Challenge Solution/CTViewController.h
@@ -0,0 +1,13 @@
+//
+//  CTViewController.h
+//  User Data Challenge Solution
+//
+//  Created by Monika Gorkani on 5/26/14.
+//  Copyright (c) 2014 Cyrus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTViewController : UIViewController
+
+@end
