9caa44f7341f41b9587191f4aec2206cdb68dfba
Cyrus Tau <cyrus.tau@gmail.com>
2014-05-28 21:51:12 -0700

    First Real Commit

---
+++ User Data Challenge Solution/CTUserData.h
@@ -0,0 +1,19 @@
+//
+//  CTUserData.h
+//  User Data Challenge Solution
+//
+//  Created by Monika Gorkani on 5/26/14.
+//  Copyright (c) 2014 Cyrus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CTUserData : NSObject
+
++(NSArray *)users;
+
+
+
+
+
+@end
---
+++ User Data Challenge Solution/CTUserData.m
@@ -0,0 +1,18 @@
+//
+//  CTUserData.m
+//  User Data Challenge Solution
+//
+//  Created by Monika Gorkani on 5/26/14.
+//  Copyright (c) 2014 Cyrus. All rights reserved.
+//
+
+#import "CTUserData.h"
+
+@implementation CTUserData
++(NSArray *)users
+{
+    NSDictionary *user1 = @{@"Username": @"LordOfNegatives", @"Email": @"mgorkani@gmail.com", @"password": @"-1cyrus", @"Age": @45, @"Profile Picture": [UIImage imageNamed:@"monika.jpg"]};
+
+}
+
+@end
